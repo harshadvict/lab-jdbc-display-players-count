86abda8562e417d8846037896ffc308df1e86a27
harshadvict <32740046+harshadvict@users.noreply.github.com>
2020-09-26 10:00:12 +0530

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,24 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+
+class Main{
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		SkillDAO skilldaoobj=new SkillDAO();
+		TreeMap<Skill,Integer> skilltree=new TreeMap<>();
+		skilltree=skilldaoobj.skillCount();
+		Iterator<Skill> iterator=skilltree.keySet().iterator();
+		while(iterator.hasNext()) {
+			Skill skillobj=iterator.next();
+			String name=skillobj.getSkillName();
+			System.out.println(skillobj.getSkillName()+"\t\t"+skilltree.get(name));
+		}
+		
+	}
+}
 
